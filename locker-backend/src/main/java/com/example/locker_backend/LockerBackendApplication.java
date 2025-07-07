84b5d6e4b56c23a49152def90415e7d4cd314413
Ben <bendanderson13@gmail.com>
2025-07-06 21:27:19 -0500

    more mnonorepo testing with backend

---
+++ locker-backend/src/main/java/com/example/locker_backend/LockerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.locker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockerBackendApplication.class, args);
+	}
+
+}
