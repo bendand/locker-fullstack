83e4b8c92b7b41547625ef2ca329553bb204cef6
Ben <bendanderson13@gmail.com>
2025-07-09 01:05:16 -0500

    more updates

--- locker-backend/src/main/java/com/example/locker_backend/controllers/UserController.java
+++ locker-backend/src/main/java/com/example/locker_backend/controllers/UserController.java
@@ -1,10 +1,47 @@
 package com.example.locker_backend.controllers;
 
+import com.example.locker_backend.models.User;
+import com.example.locker_backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/albums")
+@RequestMapping("/users")
 public class UserController {
+
+    // Autowired UserRepository to handle user data operations
+    @Autowired
+    private UserRepository userRepository;
+
+    // Endpoint to get all users
+    @GetMapping("")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Endpoint to get a user by ID
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable(value = "id") int userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
+
+    // Endpoint to add a new user
+    @PostMapping("")
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // Endpoint to update an existing user
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable(value = "id") int userId, @RequestBody User user) {
+        user.setId(userId);
+        return userRepository.save(user);
+    }
+
+    // Endpoint to delete a user by ID
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable(value = "id") int id) {
+        userRepository.findById(id).ifPresent(currUser -> userRepository.deleteById(id));
+    }
 }
--- locker-backend/src/main/java/com/example/locker_backend/models/User.java
+++ locker-backend/src/main/java/com/example/locker_backend/models/User.java
@@ -1,9 +1,8 @@
 package com.example.locker_backend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Objects;
 
 @Entity
 public class User {
@@ -12,31 +11,45 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String name;
+    @Column(name="firstName")
+    private String firstName;
+
+    @Column(name="lastName")
+    private String lastName;
+
+    @Column(name="email")
     private String email;
+
+    @Column(name="passHash")
     private String passHash;
 
-    public User(String name, String email, String passHash) {
-        this.name = name;
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
-        this.passHash = passHash;
+        this.passHash = hashCode(password);
     }
 
-    public User(int id, String name, String email, String passHash) {
+    public User(int id, String firstName, String lastName, String email, String passHash) {
         this.id = id;
-        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.passHash = passHash;
     }
 
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
     public int getId() {
         return id;
@@ -50,16 +63,30 @@ public class User {
         return passHash;
     }
 
-    public void setPassHash(String passHash) {
-        this.passHash = passHash;
+    public void setPassHash(String password) { this.passHash = hashCode(password); }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String fullName() {
+        return firstName + " " + lastName;
     }
 
-
-
-    private Boolean validateUser(String name, String passHash) {
-//        query for matching credentials
-        return null;
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " - " + email;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(passHash, user.passHash);
+    }
 }
